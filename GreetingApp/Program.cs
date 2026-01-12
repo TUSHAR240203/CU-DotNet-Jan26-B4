927d02b97b005f8da26c07bfc4dea101975f065b
Tushar Sharma <tushar.6036@gmail.com>
2026-01-12 14:28:49 +0530

    Initial commit - Greeting Solution

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreetingLibrary;
+
+namespace GreetingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            string message = Class1.GetGreeting(name);
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
