cfe89ed1b16cd250b6770bd8cb5940f51aad66a5
Tushar Sharma <tushar.6036@gmail.com>
2026-01-21 14:38:30 +0530

    day 14

---
+++ Day14/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Day14
+{
+    class Employee
+    {
+        private int id;
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public int getID()
+        {
+            return id;
+        }
+
+        public string Name { get; set; }
+
+        private string department;
+        public string Department
+        {
+            get { return department; }
+            set
+            {
+                if (value == "Accounts" || value == "Sales" || value == "IT")
+                {
+                    department = value;
+                }
+                else
+                {
+                    department = "Invalid";
+                }
+            }
+        }
+
+        private int salary;
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value < 50000 || value > 90000)
+                {
+                    salary = 50000;
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"ID : {id}");
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Department : {Department}");
+            Console.WriteLine($"Salary : {salary}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.setId(101);
+            emp.Name = "Tushar Sharma";
+            emp.Department = "Accounts";
+            emp.Salary = 100;
+            emp.Display();
+        }
+    }
+}
