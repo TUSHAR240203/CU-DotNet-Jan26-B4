7ab5378fd0df41022fe946945e5bfeb9f4d3c446
Tushar Sharma <tushar.6036@gmail.com>
2026-02-03 14:37:04 +0530

    done

---
+++ Exercises/Day24(a)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace Day24_a_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable employeeTable = new Hashtable();
+            employeeTable.Add(101, "Alice");
+            employeeTable.Add(102, "Bob");
+            employeeTable.Add(103, "Charlie");
+            employeeTable.Add(104, "Diana");
+
+            foreach (int i in employeeTable.Keys)
+            {
+                Console.WriteLine(i);
+            }
+
+            if (!employeeTable.ContainsKey(105))
+                employeeTable.Add(105, "Edward");
+            else
+                Console.WriteLine("ID already exists.");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            Console.WriteLine("After Checking");
+            foreach (int i in employeeTable.Keys)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            Console.WriteLine("Check Again");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            
+            if (!employeeTable.ContainsKey(105))
+                employeeTable.Add(105, "Edward");
+            else
+                Console.WriteLine("ID already exists.");
+
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            
+            string empName = (string)employeeTable[102];
+            Console.WriteLine("Employee 102: " + empName);
+            
+            Console.WriteLine("-------------------------------------------------------------------------------------------"); Console.WriteLine("-------------------------------------------------------------------------------------------");
+            
+            foreach (DictionaryEntry entry in employeeTable)
+                Console.WriteLine("ID: " + entry.Key + ", Name: " + entry.Value);
+            
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            
+            employeeTable.Remove(103);
+            Console.WriteLine("Total Employees: " + employeeTable.Count);
+        }
+    }
+}
---
+++ Exercises/Day24(b)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Day24_b_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double,string> leaderboard = new SortedDictionary<double,string>();
+            leaderboard.Add(55.42,"SwiftRace");
+            leaderboard.Add(52.10, "SpeedDemon");
+            leaderboard.Add(58.91, "SteadyEddie");
+            leaderboard.Add(51.05, "TurboTom");
+            foreach(var x in leaderboard) {
+                Console.WriteLine(x);
+                }
+
+            decimal Goldmedal = (decimal)leaderboard.Keys.First();
+            Console.WriteLine($"Goldmedal Timming is {Goldmedal}");
+
+            leaderboard.Remove(2);
+            leaderboard.Add(54.00, "SteadyEddie");
+            Console.WriteLine("Dict after update");
+            foreach (var x in leaderboard)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
