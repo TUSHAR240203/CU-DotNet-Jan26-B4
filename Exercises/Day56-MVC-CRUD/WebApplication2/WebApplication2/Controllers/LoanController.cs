9cb3997af45841f67a4861054fe699ec7fbd3317
Tushar Sharma <tushar.6036@gmail.com>
2026-03-17 09:48:55 +0530

    done

---
+++ Exercises/Day56-MVC-CRUD/WebApplication2/WebApplication2/Controllers/LoanController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication2.Controllers
+{
+    public class LoanController : Controller
+    {
+        private static List<Loan> loans = new List<Loan>()
+        {
+            new Loan { Id = 1, BorrowerName = "Rahul", LenderName = "ABC Bank", Amount = 50000, IsSettled = false },
+            new Loan { Id = 2, BorrowerName = "Priya", LenderName = "City Finance", Amount = 120000, IsSettled = true }
+        };
+
+        public IActionResult Index()
+        {
+            return View(loans);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Loan loan)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loan);
+            }
+
+            loan.Id = loans.Count + 1;
+            loans.Add(loan);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var loan = loans.FirstOrDefault(x => x.Id == id);
+
+            if (loan == null)
+                return NotFound();
+
+            return View(loan);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Loan loan)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loan);
+            }
+
+            var existing = loans.FirstOrDefault(x => x.Id == loan.Id);
+
+            if (existing == null)
+                return NotFound();
+
+            existing.BorrowerName = loan.BorrowerName;
+            existing.LenderName = loan.LenderName;
+            existing.Amount = loan.Amount;
+            existing.IsSettled = loan.IsSettled;
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var loan = loans.FirstOrDefault(x => x.Id == id);
+
+            if (loan != null)
+            {
+                loans.Remove(loan);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Exercises/Day56-MVC-CRUD/WebApplication2/WebApplication2/Models/Loan.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Borrower Name is required")]
+        [Display(Name = "Borrower Name")]
+        public string BorrowerName { get; set; }
+
+        public string LenderName { get; set; }
+
+        [Range(1, 500000, ErrorMessage = "Amount must be between 1 and 500000")]
+        public double Amount { get; set; }
+
+        public bool IsSettled { get; set; }
+    }
+}
